91f034cd16fc9b42eabc8f0665070d89f5fafc77
Vince Clark Tanola <vince.clark.tanola@accenture.com>
2024-01-31 11:02:24 +0800

    moved messagesender to Main -test

--- VisaAPI/Program.cs
+++ VisaAPI/Program.cs
@@ -32,20 +32,15 @@ namespace VisaAPI
 
             app.MapControllers();
 
-            app.Run();
-        }
-
-        public class privatemessagesender
-        {
             const string ServiceBusConnectionString = "Endpoint=sb://example.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AbCdEfGhIjKlMnOpQrStUvWxYz==";
             const string QueueName = "visamessages";
 
-            static void Main(string[] args)
-            {
-                Console.WriteLine("Sending a message to the Sales Messages queue...");
-                SendSalesMessageAsync().GetAwaiter().GetResult();
-                Console.WriteLine("Message was sent successfully.");
-            }
+            //static void Main(string[] args)
+            //{
+            //    Console.WriteLine("Sending a message to the Sales Messages queue...");
+            //    SendSalesMessageAsync().GetAwaiter().GetResult();
+            //    Console.WriteLine("Message was sent successfully.");
+            //}
 
             static async Task SendSalesMessageAsync()
             {
@@ -71,7 +66,49 @@ namespace VisaAPI
                     await client.DisposeAsync();
                 }
             }
+
+            app.Run();
+
         }
+
+
+        //public class Privatemessagesender
+        //{
+        //    const string ServiceBusConnectionString = "Endpoint=sb://example.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AbCdEfGhIjKlMnOpQrStUvWxYz==";
+        //    const string QueueName = "visamessages";
+
+        //    //static void Main(string[] args)
+        //    //{
+        //    //    Console.WriteLine("Sending a message to the Sales Messages queue...");
+        //    //    SendSalesMessageAsync().GetAwaiter().GetResult();
+        //    //    Console.WriteLine("Message was sent successfully.");
+        //    //}
+
+        //    static async Task SendSalesMessageAsync()
+        //    {
+        //        await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+        //        await using ServiceBusSender sender = client.CreateSender(QueueName);
+        //        try
+        //        {
+        //            string messageBody = $"A message for Visa from privatemessagesender.cs";
+        //            var message = new ServiceBusMessage(messageBody);
+        //            Console.WriteLine($"Sending message: {messageBody}");
+        //            await sender.SendMessageAsync(message);
+        //        }
+        //        catch (Exception exception)
+        //        {
+        //            Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+        //        }
+        //        finally
+        //        {
+        //            // Calling DisposeAsync on client types is required to ensure that network
+        //            // resources and other unmanaged objects are properly cleaned up.
+        //            await sender.DisposeAsync();
+        //            await client.DisposeAsync();
+        //        }
+        //    }
+        //}
     }
 }
 
